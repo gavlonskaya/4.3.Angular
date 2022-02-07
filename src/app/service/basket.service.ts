006c0239966ae674268f8ffe7001634e411ba3bc
Gavlonskaya Ekaterina <gavlonskaya.katerina.yandex.ru>
2022-02-08 00:16:27 +0300

    commit

---
+++ src/app/service/basket.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { cardMock } from "src/app/mock/card.mock";
+import { Card } from "src/app/model/card.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BasketService{
+  private toBasket: Card[]=[];
+
+  moveToCart(buyCard: Card): void{
+    this.toBasket.push({...buyCard, buy: true});
+      
+  }
+  getCartData(): Card[]{
+    return this.toBasket;
+  }
+}
---
+++ src/app/service/card.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+import { cardMock } from "src/app/mock/card.mock";
+import { Card } from "src/app/model/card.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CardsService{
+  private products: Card[] = cardMock;
+
+  getCards(): Card[]{
+    return this.products;
+  }
+  getCard(id: number): Card| null{
+     const product = this.products.find((item: Card)=>{
+      return item.id === id;
+    });
+     return product? product: null;
+  }
+
+
+
+  getActiveCard(search:string = ''): Card[]{
+    return this.products.filter((item:Card)=>{
+      return item.title.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+      || item.price.toLocaleLowerCase().includes(search.toLocaleLowerCase());
+    });
+  }
+}
